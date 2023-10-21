e1b60734bc6134ab330af4569a4c97a7d3274ceb
Baby Feavel <feavel@qq.com>
2023-10-21 15:40:02 +0800

    ok

--- src/routes/key-vision/mother's-day/+page.svelte
+++ src/routes/key-vision/mother's-day/+page.svelte
@@ -1,5 +1,5 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import mothersDay1 from "$lib/assets/key-vision/mother's-day/mothers-day-1.png";
 </script>
 
-<img src={mothersDay1} />
+<img src={mothersDay1} alt="Mother's day" /> -->
